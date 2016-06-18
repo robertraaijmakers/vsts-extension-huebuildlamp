18934b1b969f784108e25552466be4766dd9fbb6
robertraaijmakers <robertraaijmakers@gmail.com>
2016-06-18 19:24:38 +0200

    Updated Hue Script with added support for IFTTT Hashtag mails. Updated input parameters with logic groups Added screenshots to Marketplace view

--- vsts-extension-huebuildlamp/PhilipsHueIfttt/PhilipsHueIfttt.ps1
+++ vsts-extension-huebuildlamp/PhilipsHueIfttt/PhilipsHueIfttt.ps1
@@ -4,18 +4,46 @@
 [CmdletBinding(DefaultParameterSetName = 'None')]
 param(
 	[string][Parameter(Mandatory=$true)] $sendMailFrom,
-	[string][Parameter(Mandatory=$true)] $sendMailTo,
+	#[string][Parameter(Mandatory=$true)] $smtpServerEndpoint,
 	[string][Parameter(Mandatory=$true)] $smtpServer,
-	[string][Parameter(Mandatory=$true)] $smtpPortVariable,
 	[string][Parameter(Mandatory=$true)] $smtpUserName,
 	[string][Parameter(Mandatory=$true)] $smtpPassword,
 	[string][Parameter(Mandatory=$true)] $smtpUseSslVariable,
-	[string] $buildNames
+	[string][Parameter(Mandatory=$true)] $smtpPortVariable,
+	[string] $buildNames,
+	[string][Parameter(Mandatory=$true)] $sendMailTo,
+	[string] $mailHashtag
 )
+
+<#$sendMailFrom = Get-VstsInput -Name sendMailFrom -Require
+$smtpServerEndpoint = Get-VstsInput -Name smtpServerEndpoint -Require
+$smtpServer = Get-VstsInput -Name smtpServer -Require
+$smtpUserName = Get-VstsInput -Name smtpUserName -Require
+$smtpPassword = Get-VstsInput -Name smtpPassword -Require
+$smtpUseSslVariable = Get-VstsInput -Name smtpUseSslVariable -Require
+$smtpPortVariable = Get-VstsInput -Name smtpPortVariable -Require
+$buildNames = Get-VstsInput -Name buildNames
+$sendMailTo = Get-VstsInput -Name sendMailTo -Require
+$mailHashtag = Get-VstsInput -Name mailHashtag
+
+# Retrieves the url, username and password from the specified generic endpoint.
+function GetEndpointData
+{
+	param([string][ValidateNotNullOrEmpty()]$smtpServerEndpoint)
+
+	$serviceEndpoint = Get-VstsEndpoint -Name $smtpServerEndpoint -Require
+
+	if (!$serviceEndpoint)
+	{
+		throw "A Connected Service with name '$smtpServerEndpoint' could not be found. Ensure that this Connected Service was successfully provisioned using the services tab in the Admin UI."
+	}
+
+   return $serviceEndpoint
+}#>
 
 Write-Host "Starting Control Philips Hue Task via IFTTT"
 
-<#TODO: Validate global variables#>
+# TODO: Validate global variables
 [string]$serverUrl = $Env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI
 [string]$projectName = $Env:SYSTEM_TEAMPROJECT
 [int]$smtpPort = [convert]::ToInt32($smtpPortVariable, 10)
@@ -23,7 +51,16 @@ Write-Host "Starting Control Philips Hue Task via IFTTT"
 [securestring]$securePassword = convertto-securestring $smtpPassword -asplaintext -force
 [string]$scriptPath = Split-Path -parent $PSCommandPath
 
-<#Built email message#>
+# Split URL in port and URL if possible
+<#$serviceEndpoint = GetEndpointData $smtpServerEndpoint
+$smtpServer = "$($serviceEndpoint.Url)"
+$smtpPort = $serviceEndpoint.Url.Port
+Write-Host "SMTP server url set to $($smtpServer)"
+Write-Host "SMTP port set to $($serviceEndpoint.Url.Port)"
+$smtpUserName = $serviceEndpoint.Auth.Parameters.Username
+$securePassword = $serviceEndpoint.Auth.Parameters.Password#>
+
+# Built email message
 $message = New-Object System.Net.Mail.MailMessage
 $message.From = $sendMailFrom
 $message.To.add($sendMailTo)
@@ -36,11 +73,11 @@ $fileUrl = "green.jpg"
 [string]$user = ""
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $user,$Env:SYSTEM_ACCESSTOKEN)))
 
-# Construct the REST URL to obtain Build ID
+# Construct the REST URL to obtain Build Definitions
 $uri = "{0}{1}/_apis/build/definitions?api-version=2.0" -f $serverUrl,[uri]::EscapeDataString($projectName)
 $buildDefinitions = Invoke-RestMethod -Uri $uri -Method Get -ContentType "application/json" -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)}
  
-# This call should only provide a single result; Capture the Build ID from the result
+# Fail if no build definitions found.
 if ($buildDefinitions.count -eq 0)
 {
      throw "No build definitions found."
@@ -140,6 +177,11 @@ foreach($buildResult in $buildResults.value)
 
 $mailAttachment = new-object Net.Mail.Attachment("$($scriptPath)/$($fileUrl)")
 $message.Attachments.Add($mailAttachment)
+
+if($mailHashtag)
+{
+	$message.Subject = "$($message.Subject) #$($mailHashtag)"
+}
 
 <#Sending message to IFTTT#>
 write-Host "Sending email message"
